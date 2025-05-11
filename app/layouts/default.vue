f1f67a06088a6a30eb24d0d10c39eaeda549a684
Andrea Binelli <binelli.andrea@gmail.com>
2025-05-11 16:48:26 +0200

    refactor: update AppFooter links and modify default theme to light

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,60 @@
+<script
+  setup
+  lang="ts"
+>
+const drawer = ref(false);
+const router = useRouter();
+
+// Get all routes and filter out index.vue
+const pageRoutes = computed(() => {
+  return router
+    .getRoutes()
+    .filter(route => route.name !== 'index')
+    .sort((a, b) => {
+      const nameA = String(a.name);
+      const nameB = String(b.name);
+      return nameA.localeCompare(nameB);
+    })
+});
+
+// Format route name to capitalized title
+// Example: "add-random" becomes "Add Random"
+const formatRouteTitle = (route: RouteRecordNormalized) => {
+  const routeName = String(route.name);
+  return routeName
+    .split('-')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+};
+
+
+</script>
+
 <template>
   <v-app>
+    <v-navigation-drawer v-model="drawer">
+      <v-list>
+        <v-list-item
+          v-for="pageRoute in pageRoutes"
+          :key="pageRoute.path"
+          :to="pageRoute.path"
+          :title="formatRouteTitle(pageRoute)"
+        />
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+
+      <v-app-bar-title>FlowLess</v-app-bar-title>
+      
+      <v-spacer/>
+      
+      <v-btn icon to="/" title="Home">
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+    </v-app-bar>
+
     <v-main>
       <slot />
     </v-main>
@@ -7,3 +62,23 @@
     <AppFooter />
   </v-app>
 </template>
+
+<!-- <style>
+@import '@vue-flow/core/dist/style.css';
+@import '@vue-flow/core/dist/theme-default.css';
+@import '@vue-flow/controls/dist/style.css';
+
+html,
+body,
+#__nuxt {
+  margin: 0;
+  height: 100%;
+}
+
+#__nuxt {
+  text-transform: uppercase;
+  font-family: 'JetBrains Mono', monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style> -->
