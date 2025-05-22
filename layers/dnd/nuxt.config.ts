a85e41cc6af68c71134946b304f8b939ea7ebb70
Andrea Binelli <binelli.andrea@gmail.com>
2025-05-22 23:35:48 +0200

    refactor: remove custom Nuxt configuration for drag and drop example

--- layers/dnd/nuxt.config.ts
+++ layers/dnd/nuxt.config.ts
@@ -1,9 +1,1 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  // Extend the main Nuxt 3 configuration for this example
-  app: {
-    head: {
-      title: 'Vue Flow Drag and Drop',
-    },
-  },
-})
+export default defineNuxtConfig({});
