5c7382b5e26b2af8d5727f59c7a6d147fe47bd49
Andrea Binelli <binelli.andrea@gmail.com>
2025-05-24 00:28:48 +0200

    feat: add hidden layer example with toggle functionality for nodes and edges

---
+++ layers/hidden/pages/example/hidden.vue
@@ -0,0 +1,36 @@
+<script
+  setup
+  lang="ts"
+>
+import { Background } from '@vue-flow/background'
+import { Panel, VueFlow } from '@vue-flow/core'
+import { hiddenInitialNodes, hiddenInitialEdges } from '../../assets/hidden-initial-elements.js'
+
+const { nodes, edges, isHidden, toggleHidden } = useHiddenToggle(hiddenInitialNodes, hiddenInitialEdges)
+
+definePageMeta({
+  title: 'Hidden Example',
+  description: 'Vue Flow example demonstrating how to hide/show nodes and edges'
+})
+</script>
+
+<template>
+  <VueFlow
+    :nodes="nodes"
+    :edges="edges"
+    fit-view-on-init
+  >
+    <Background />
+
+    <Panel position="top-right">
+      <HiddenToggleButton
+        :is-hidden="isHidden"
+        @toggle="toggleHidden"
+      />
+    </Panel>
+  </VueFlow>
+</template>
+
+<style>
+@import '../../assets/hidden-style.css';
+</style>
