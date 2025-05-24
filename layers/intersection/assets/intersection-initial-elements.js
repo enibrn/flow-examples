fee7b16f082e72119f7f16d7ba02192918f9fb13
Andrea Binelli <binelli.andrea@gmail.com>
2025-05-24 11:52:58 +0200

    feat: add intersection detection example with nodes, panel, and styles

---
+++ layers/intersection/assets/intersection-initial-elements.js
@@ -0,0 +1,28 @@
+export const initialNodes = [
+  {
+    id: '1',
+    data: { label: 'Node 1' },
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: '2',
+    data: { label: 'Node 2' },
+    position: { x: 400, y: 400 },
+  },
+  {
+    id: '3',
+    data: { label: 'Node 3' },
+    position: { x: 400, y: 0 },
+  },
+  {
+    id: '4',
+    data: { label: 'Node 4' },
+    position: { x: 0, y: 400 },
+  },
+  {
+    id: '5',
+    style: { borderColor: 'red' },
+    data: { label: 'Drag me  over another node' },
+    position: { x: 200, y: 200 },
+  },
+]
